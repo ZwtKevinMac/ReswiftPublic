e7fef6460ffc8de46d8f9de6500693b30085cb99
hinalzwt <43809650+hinalzwt@users.noreply.github.com>
2018-10-03 17:34:49 +0530

    first Commit Hinal

---
+++ CounterViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CounterViewController.swift
+//  ReswiftDemo
+//
+//  Created by Hinal on 03/10/18.
+//  Copyright © 2018 Hinal. All rights reserved.
+//
+
+import UIKit
+
+class CounterViewController: UIViewController {
+
+    @IBOutlet var lblCounter: UILabel!
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+    }
+    
+    @IBAction func incrementAction(_ sender: Any) {
+        
+    }
+    
+    @IBAction func decrementAction(_ sender: Any) {
+        
+        
+    }
+    
+}
